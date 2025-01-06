f178c8f6ebc3b1c5caff7f325890f82ffa652a43
Len Rich <lenrich1403@gmail.com>
2025-01-06 12:57:41 +0800

    refactor: ContactMe Component

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,5 +1,9 @@
+'use client'
 import classNames from "classnames";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+import { Boxes } from "../background/boxes";
+import MagicButton from "../MagicButton";
 
 type GridType = {
   children: ReactNode;
@@ -62,8 +66,26 @@ function GridItem({
 }
 
 function TechStack() {
-  const leftLists = ["ReactJS", "NextJS", "Typescript", "TailwindCSS", "Bootstrap", "Shadcn UI", "Acternity UI", "React Native"];
-  const rightLists = ["Python", "MySQL", "PostgreSQL", "Prisma", "Docker", "Linux", "AWS", "Supabase"];
+  const leftLists = [
+    "ReactJS",
+    "NextJS",
+    "Typescript",
+    "TailwindCSS",
+    "Bootstrap",
+    "Shadcn UI",
+    "Acternity UI",
+    "React Native",
+  ];
+  const rightLists = [
+    "Python",
+    "MySQL",
+    "PostgreSQL",
+    "Prisma",
+    "Docker",
+    "Linux",
+    "AWS",
+    "Supabase",
+  ];
   return (
     <div className="grid grid-cols-2 items-center justify-center gap-6 h-full">
       <div className="p-4 text-center">
@@ -104,6 +126,32 @@ function TechStack() {
   );
 }
 
+function ContactMe() {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    const text = "lenrich1403@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
+
+  return (
+    <div className="h-full relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg px-8 lg:p-0">
+      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <Boxes />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h3 className="text-white text-2xl text-wrap text-center z-50">Do you want to start a project together?</h3>
+        <MagicButton
+          title={copied ? "Email is Copied!" : "Copy my email address"}
+          icon={<IoCopyOutline />}
+          position="left"
+          handleClick={handleCopy}
+          otherClasses="!bg-[#161A31]"
+        />
+      </div>
+    </div>
+  );
+}
+
 const About = () => {
   return (
     <div id="about" className="mt-24">
@@ -116,7 +164,9 @@ const About = () => {
           <GridItem className="md:col-span-3">3</GridItem>
         </Grid>
         <Grid cols={1} gap={6} className="md:grid-cols-3">
-          <GridItem className="md:col-span-1">4</GridItem>
+          <GridItem className="md:col-span-1 !p-0">
+            <ContactMe />
+          </GridItem>
           <GridItem className="md:col-span-1">5</GridItem>
           <GridItem className="md:col-span-1">6</GridItem>
         </Grid>
